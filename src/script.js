aded18d4de04178bba866db91c8ffe19d81220fa
mary ann <114210058+maryannintech@users.noreply.github.com>
2025-06-30 18:54:44 +0800

    refactor: simplify user input handling on coming soon page

--- src/script.js
+++ src/script.js
@@ -9,23 +9,11 @@ const userNameElement = document.querySelector(".user-name");
 const userNameInput = document.querySelector(".user-input");
 
 if (userNameElement && userNameInput) {
-  setTimeout(() => {
-    userNameElement.textContent = "";
-  }, 5000);
   userNameInput.addEventListener("keydown", (event) => {
-    if (
-      event.key === "Backspace" ||
-      event.key === "Delete" ||
-      event.key === "Escape" ||
-      event.key === "Enter" ||
-      event.key === "Tab" ||
-      event.key === "Shift" ||
-      event.key === "Control" ||
-      event.key === "Alt"
-    ) {
-      userNameElement.textContent = userNameElement.textContent.slice(0, -1);
-      return;
+    if (event.key === "Enter") {
+      event.preventDefault();
+      userNameElement.textContent = userNameInput.value;
+      userNameInput.value = "";
     }
-    userNameElement.textContent += event.key;
   });
 }
