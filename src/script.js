a66bffc1d4412ad65e1b385317dea63c8cdb33e4
mary ann <114210058+maryannintech@users.noreply.github.com>
2025-06-30 18:48:19 +0800

    feat: enhance coming soon page with user interaction features and improved styles

--- src/script.js
+++ src/script.js
@@ -1,3 +1,22 @@
-document.querySelector(".card-container").addEventListener("click", () => {
+const cardHTML = document.querySelector(".card-container");
+
+if (cardHTML) {
+  cardHTML.addEventListener("click", () => {
     window.location.href = "coming-soon.html";
-});
+  });
+}
+const userNameElement = document.querySelector(".user-name");
+const userNameInput = document.querySelector(".user-input");
+
+if (userNameElement && userNameInput) {
+    setTimeout(() => {
+    userNameElement.textContent = "";
+    }, 5000)
+  userNameInput.addEventListener("keydown", (event) => {
+    if (event.key === "Backspace") {
+      userNameElement.textContent = userNameElement.textContent.slice(0, -1);
+      return;
+    }
+    userNameElement.textContent += event.key;
+  });
+}
